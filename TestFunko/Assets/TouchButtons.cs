830440eac2e532c58442225af55bd4201e54bc1b
Juan Cerda <iscjuancerda@gmail.com>
2016-10-26 22:17:20 -0500

    Fixed Analog stick and added Touch Buttons, need to implement touch detection for on-screen buttons

---
+++ TestFunko/Assets/TouchButtons.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class TouchButtons : MonoBehaviour {
+
+	// Use this for initialization
+
+	public bool isTouchedDown;
+	public bool isTouched;
+
+	public string Name;
+
+	void Start()
+	{
+
+
+	}
+
+	void Update()
+	{
+
+		if(Input.touches.Length > 0)
+		{
+
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		
+	}
+}
--- TestFunko/Assets/scripts/TouchPadInput.cs
+++ TestFunko/Assets/scripts/TouchPadInput.cs
@@ -8,6 +8,10 @@ public class TouchPadInput : MonoBehaviour {
 	public GameObject referenceGameObject;
 	public GameObject arrowPoint;
 
+
+	public TouchButtons[] buttons;
+
+
 	public Player playerToControl; 
 
 	void Awake()
@@ -20,8 +24,9 @@ public class TouchPadInput : MonoBehaviour {
 		
 	void Update()
 	{
+		CheckForButtonPress();
+
 		CheckForMovementTouch();
-		ResetButtons();
 	}
 
 	bool isMovementFingerOn;
@@ -31,17 +36,17 @@ public class TouchPadInput : MonoBehaviour {
 	{
 		#region MovementInput
 
-//	    if(Input.touches.Length > 0)
-//		{
-//
-//			Touch touch = Input.GetTouch(0);
-//			Vector3 fingerPos = touch.position;
-
-		if(Input.GetMouseButton(0))
+	    if(Input.touches.Length > 0)
 		{
-			
-			Vector3 touch = Input.mousePosition;
-			Vector3 fingerPos = touch;
+
+			Touch touch = Input.GetTouch(0);
+			Vector3 fingerPos = touch.position;
+
+		//TODO: Mouse emulation of touch
+//		if(Input.GetMouseButton(0))
+//		{
+//			Vector3 touch = Input.mousePosition;
+//			Vector3 fingerPos = touch;
 
 		 	Camera cam = Camera.main;		
 			//Posicionar el vector en el lugar donde el usuario tiene el dedo mas .5 unidades para ver el objeto
@@ -70,9 +75,10 @@ public class TouchPadInput : MonoBehaviour {
 			originPoint.transform.position = objPos;
 
 			//TODO: Touchpad
-			//if(touch.position.x < Screen.width && touch.position.y < Screen.height )
-			if(touch.x < Screen.width && touch.x > 0 &&
-				touch.y < Screen.height && touch.y > 0)
+			if(touch.position.x < Screen.width /2 && touch.position.x > 0 &&
+				touch.position.y < Screen.height && touch.position.y > 0)
+			//if(touch.x < Screen.width && touch.x > 0 &&
+			//	touch.y < Screen.height && touch.y > 0)
 			{
 				isMovementFingerOn = true;
 				arrowPoint.transform.LookAt(originPoint.transform.position);
@@ -92,24 +98,16 @@ public class TouchPadInput : MonoBehaviour {
 		ActivateObjects(isMovementFingerOn);
 
 		//TODO: Touchpad
-		//lastTouchCount = Input.touches.Length;
+		lastTouchCount = Input.touches.Length;
 
 		//TODO: Mouse
-		lastTouchCount = Input.GetMouseButton(0) ? 1 : 0 ;
+		//lastTouchCount = Input.GetMouseButton(0) ? 1 : 0 ;
 
 		if(originPoint)
 		{
 			if(originPoint.activeInHierarchy && arrowPoint.activeInHierarchy)
 			{
-				Vector3 offset =  originPoint.transform.position - arrowPoint.transform.position;
-				offset.Normalize();
 
-				Camera cam = Camera.main;
-
-
-
-
-				float distance = offset.magnitude;
 
 //				Debug.DrawRay(referenceGameObject.transform.position, referenceGameObject.transform.up, Color.green, 3.0f);
 //				Debug.DrawRay(referenceGameObject.transform.position, referenceGameObject.transform.right, Color.red, 3.0f);
@@ -125,13 +123,26 @@ public class TouchPadInput : MonoBehaviour {
 //
 //				Debug.Log("Dot Product yellow and green" + Vector3.Dot(arrowPoint.transform.forward, referenceGameObject.transform.up));
 
-				Vector3 direction = offset / distance;
+				Camera cam = Camera.main;
+				Vector3 originPosToCam = cam.WorldToScreenPoint(originPoint.transform.position);
+				Vector3 arrowPosToCam = cam.WorldToScreenPoint(arrowPoint.transform.position);
 
-				float x = direction.x;
-				float y = direction.y;
+				Vector3 _2dOffset = originPosToCam - arrowPosToCam;
+				float _2dDistance = _2dOffset.magnitude;
 
-				MovementAxis_Horizontal = x > 0 ? 1 : -1;
-				MovementAxis_Vertical = y > 0 ? 1 : -1;
+				//Debug.Log("2dDistance" + _2dDistance);
+
+				Vector3 _2dDirection = _2dOffset / _2dDistance;
+
+				float x = _2dDirection.x;
+				float y = _2dDirection.y;
+				float z = _2dDirection.z;
+
+				Debug.Log("X:" + x + "   Y: " + y +  "   Z: " + z);
+
+
+				MovementAxis_Horizontal = x ;
+				MovementAxis_Vertical = y ;
 
 			}
 			else
@@ -145,6 +156,15 @@ public class TouchPadInput : MonoBehaviour {
 
 	}
 
+	void CheckForButtonPress()
+	{
+
+		
+		
+
+
+	}
+
 	void ActivateObjects(bool Activate)
 	{
 
@@ -153,8 +173,7 @@ public class TouchPadInput : MonoBehaviour {
 			originPoint.SetActive(true);
 			arrowPoint.SetActive(true);
 
-			//0.3f Magic Number: means the origin will be positioned a little bit below, since don't want to catch an unintended crouch position
-			//originPoint.transform.position = new Vector3(positionInWorldPoints.x, positionInWorldPoints.y -0.3f, 0.0f);
+
 		}
 		else if(!Activate)
 		{
@@ -169,44 +188,8 @@ public class TouchPadInput : MonoBehaviour {
 		}
 	}
 
-
-	public void CheckForButtonInput(int ButtonNumber)
-	{
-		if(ButtonNumber == 1) Button1 = true; else Button1 = false; 
-		if(ButtonNumber == 2) Button2 = true; else Button2 = false; 
-		if(ButtonNumber == 3) Button3 = true; else Button3 = false;
-		if(ButtonNumber == 4) Button4 = true; else Button4 = false;
-		if(ButtonNumber == 5) Button5 = true; else Button5 = false;
-		if(ButtonNumber == 6) Button6 = true; else Button6 = false;
-		if(ButtonNumber == 7) Button7 = true; else Button7 = false;
-		if(ButtonNumber == 8) Button8 = true; else Button8 = false;
-		if(ButtonNumber == 9) Button9 = true; else Button9 = false;
-	}
-
-	void ResetButtons()
-	{
-		Button1 = false;
-		Button2 = false;
-		Button3 = false;
-		Button4 = false;
-		Button5 = false;
-		Button6 = false;
-		Button7 = false;
-		Button8 = false;
-		Button9 = false;
-	}
-
 	public static float MovementAxis_Vertical;
 	public static float MovementAxis_Horizontal;
 
-	public static bool Button1;
-	public static bool Button2;
-	public static bool Button3;
-	public static bool Button4;
 
-	public static bool Button5;
-	public static bool Button6;
-	public static bool Button7;
-	public static bool Button8;
-	public static bool Button9;
 }
